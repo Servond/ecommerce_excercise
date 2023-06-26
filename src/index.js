52633f83b41699924a2a0aa6e14adbd3d852703a
Bryan Karta <karta.bryan@gmail.com>
2023-06-26 14:34:11 +0700

    change browser router to hash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from 'react-redux';
 
